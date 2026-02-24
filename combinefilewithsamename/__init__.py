af4155efe140bc027d43d63b5bc5c224ea7efd94
jeet071992-png <jeet071992@gmail.com>
2026-02-24 10:15:34 +0530

    Delete combinefilewithsamename directory

--- combinefilewithsamename/__init__.py
+++
@@ -1,5 +0,0 @@
-from .core import run, inject_vba, combine_excel_files
-
-__version__ = "1.0.0"
-__author__ = "jeet071992-png"
-__all__ = ["run", "inject_vba", "combine_excel_files"]
--- combinefilewithsamename/combinefilewithsamename/__main__.py
+++
@@ -1,4 +0,0 @@
-from combinefilewithsamename import run
-
-if __name__ == "__main__":
-    run()
